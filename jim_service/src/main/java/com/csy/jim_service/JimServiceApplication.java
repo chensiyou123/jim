14228d9cc3a28ce2c58adc9ebab780eea32f592f
90563 <905630790@qq.com>
2019-08-26 16:16:00 +0800

    feat:"添加配置"

--- jim_service/src/main/java/com/csy/jim_service/JimServiceApplication.java
+++ jim_service/src/main/java/com/csy/jim_service/JimServiceApplication.java
@@ -1,5 +1,7 @@
 package com.csy.jim_service;
 
+import org.jim.common.ImConfig;
+import org.jim.common.config.PropertyImConfigBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JimServiceApplication {
 
     public static void main(String[] args) {
+        ImConfig imConfig = new PropertyImConfigBuilder("jim.properties").build();
         SpringApplication.run(JimServiceApplication.class, args);
     }
 
