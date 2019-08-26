aa2076a3982cc5c2ac0979c162904e1fc1a9b463
90563 <905630790@qq.com>
2019-08-26 16:05:38 +0800

    feat:"初始化项目"

---
+++ jim_service/src/main/java/com/csy/jim_service/JimServiceApplication.java
@@ -0,0 +1,13 @@
+package com.csy.jim_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimServiceApplication.class, args);
+    }
+
+}
