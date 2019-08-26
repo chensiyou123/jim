de91309e42e19c843298dcb15ab229f2ec031ff8
90563 <905630790@qq.com>
2019-08-26 17:10:04 +0800

    feat:"添加配置信息"

---
+++ jim_service/src/main/java/com/csy/jim_service/controller/indexController.java
@@ -0,0 +1,13 @@
+package com.csy.jim_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/")
+public class indexController {
+
+    @GetMapping
+    public String index(){
+        return "/page1/index";
+    }
+}
